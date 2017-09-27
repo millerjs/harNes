b855c0d269b6c89b4280c7b5ba87b17a8aa5c3e9
Joshua Miller <jsmiller@uchicago.edu>
2017-09-26 22:26:40 -0500

    add test module stub

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,9 @@
 #[macro_use]
 extern crate lazy_static;
 
+#[cfg(test)]
+mod tests;
+
 const MOS_6502_CLOCK_RATE: usize = 1789773;
 const MOS_6502_MEMORY_SIZE: usize = 65536;
 
@@ -20,7 +23,7 @@ impl Memory for Ram {
         0
     }
 
-    fn write(&self, address: Address, value: Word) -> Word {
+    fn write(&mut self, address: Address, value: Word) -> Word {
         0
     }
 }
@@ -62,34 +65,34 @@ impl Default for Ram {
 
 pub trait Memory: Default {
     fn read(&self, address: Address) -> Word;
-    fn write(&self, address: Address, value: Word) -> Word;
+    fn write(&mut self, address: Address, value: Word) -> Word;
 }
 
 #[no_mangle]
 #[derive(Default)]
 pub struct Flags {
-    carry: bool,
-    zero: bool,
-    interupt_disable: bool,
-    decimal_mode_flag: bool,
-    break_mode: bool,
-    unused: bool,
-    overflow: bool,
-    negative: bool,
+    pub carry: bool,
+    pub zero: bool,
+    pub interupt_disable: bool,
+    pub decimal_mode_flag: bool,
+    pub break_mode: bool,
+    pub unused: bool,
+    pub overflow: bool,
+    pub negative: bool,
 }
 
 #[derive(Default)]
 pub struct Cpu<M> {
     memory: Box<M>,
     cycles: usize,
-    program_counter: Pointer,
-    stack_pointer: Word,
-    accumulator: Word,
-    register_x: Word,
-    register_y: Word,
+    pub program_counter: Pointer,
+    pub stack_pointer: Word,
+    pub accumulator: Word,
+    pub register_x: Word,
+    pub register_y: Word,
     interrupt: Word,
-    stall: usize,
-    flags: Flags,
+    pub stall: usize,
+    pub flags: Flags,
 }
 
 macro_rules! is {
