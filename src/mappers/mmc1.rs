93275ea097711a2d005ea4763d33e308f4c71eb1
Joshua Miller <jsmiller@uchicago.edu>
2018-01-26 12:46:05 -0600

    wip mmc1 mapper

--- src/mappers/mmc1.rs
+++ src/mappers/mmc1.rs
@@ -4,12 +4,14 @@ use ::memory::*;
 use ::mappers::Mapper;
 use ::cartridge::Cartridge;
 
-#[derive(Default)]
 pub struct MMC1 {
+    program_ram: Vec<Byte>,
     program_rom: Vec<Byte>,
     character_rom: Vec<Byte>,
 }
 
+const PROGRAM_RAM_SIZE: usize = 8191;
+const PROGRAM_ROM_SIZE: usize = 16383;
 
 enum Bank {
     ProgramRam,
@@ -17,14 +19,13 @@ enum Bank {
     CharacterRom,
 }
 
-
 impl Bank {
     fn from_address(address: Word) -> Bank {
         match address {
             address if address > 0x6000 && address < 0x7FFF => Bank::ProgramRam,
             address if address > 0x8000 && address < 0xBFFF => Bank::ProgramRom,
-            address if address > 0xC000 && address < 0xFFFF => Bank::ProgramRam,
-            address if address > 0x0000 && address < 0x1FFF => Bank::ProgramRam,
+            address if address > 0xC000 && address < 0xFFFF => Bank::CharacterRom,
+            address if address > 0x0000 && address < 0x1FFF => Bank::CharacterRom,
             _ => panic!("invalid mapper address {}", address)
         }
     }
@@ -32,7 +33,11 @@ impl Bank {
 impl Memory for MMC1 {
     fn read(&self, address: Word) -> Byte {
         trace!("Reading {:#x} from MMC1", address);
-        0
+        match Bank::from_address(address) {
+            Bank::ProgramRam => self.program_ram[address as usize & PROGRAM_ROM_SIZE],
+            Bank::ProgramRom => unimplemented!(),
+            Bank::CharacterRom => unimplemented!(),
+        }
     }
 
     fn write(&mut self, address: Word, value: Byte) {
@@ -46,5 +51,6 @@ pub fn from_cartridge(cartridge: Cartridge) -> Box<MMC1> {
     Box::new(MMC1 {
         program_rom: cartridge.program_memory,
         character_rom: cartridge.character_memory,
+        program_ram: vec![0; 0],
     })
 }
